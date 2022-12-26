d4c4f79081badd3c368cede3b264a316aa1df3c3
FaberVitale <faber.vitale@gmail.com>
2022-12-26 12:46:17 +0100

    feat(content): add december blog post and update hp content

--- src/pages/api/hp/en.json.ts
+++ src/pages/api/hp/en.json.ts
@@ -1,23 +1,18 @@
+import { l10n } from '@i18n/config';
 import type { HpContent } from '../../../@types/hp';
+
+const lng = 'en';
 
 export const enHpContent: Readonly<HpContent> = {
   title: 'The Javascript community in Rome!',
   description: 'We meet locally & online every 3rd Wednesday of the month',
   sections: [
     {
-      heading: 'Coming up next',
+      heading: l10n('latestTalkTitle', { lng }),
       body: 'Astro with Fabio Biondi - RomaJS #AperiTech Xmas 2022',
       cta: {
-        href: 'https://www.youtube.com/watch?v=2Vp1ATBkdIQ',
-        text: 'Register',
-      },
-    },
-    {
-      heading: 'Last meetup',
-      body: 'Our experience with TailwindCSS',
-      cta: {
-        href: 'https://www.youtube.com/watch?v=S6gUGJLJ5tA',
-        text: 'Watch the VOD',
+        href: 'https://www.youtube.com/@RomaJS/streams',
+        text: l10n('ctaVOD', { lng }),
       },
     },
   ],
