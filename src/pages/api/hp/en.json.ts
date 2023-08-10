fb254f95a58addbbe98bf0b3e3f70020866b284f
FaberVitale <faber.vitale@gmail.com>
2023-08-10 13:06:45 +0200

    fix: broken HP l10ns

--- src/pages/api/hp/en.json.ts
+++ src/pages/api/hp/en.json.ts
@@ -3,31 +3,33 @@ import type { HpContent } from '../../../@types/hp';
 
 const lng = 'en';
 
-export const enHpContent: Readonly<HpContent> = {
-  title: 'The Javascript community in Rome!',
-  description: 'We meet locally & online every 3rd Wednesday of the month',
-  sections: [
-    {
-      heading: l10n('nextTalkTitle', { lng }),
-      body: 'How Open Source changed my life & Video editing con js - RomaJS @ Sourcesense',
-      cta: {
-        href: 'https://www.meetup.com/romajs/events/293424465/',
-        text: l10n('ctaRegister', { lng }),
+export async function getEnHpContent(): Promise<Readonly<HpContent>> {
+  return {
+    title: 'The Javascript community in Rome!',
+    description: 'We meet locally & online every 3rd Wednesday of the month',
+    sections: [
+      {
+        heading: l10n('nextTalkTitle', { lng }),
+        body: 'How Open Source changed my life & Video editing con js - RomaJS @ Sourcesense',
+        cta: {
+          href: 'https://www.meetup.com/romajs/events/293424465/',
+          text: l10n('ctaRegister', { lng }),
+        },
       },
-    },
-    {
-      heading: l10n('latestTalkTitle', { lng }),
-      body: 'Attack on NPM with Alessandro Miliucci',
-      cta: {
-        href: 'https://youtu.be/BSKXpTFzKf4?t=384',
-        text: l10n('ctaVOD', { lng }),
+      {
+        heading: l10n('latestTalkTitle', { lng }),
+        body: 'Attack on NPM with Alessandro Miliucci',
+        cta: {
+          href: 'https://youtu.be/BSKXpTFzKf4?t=384',
+          text: l10n('ctaVOD', { lng }),
+        },
       },
-    },
-  ],
-};
+    ],
+  };
+}
 
 export async function get() {
   return {
-    body: JSON.stringify(enHpContent),
+    body: JSON.stringify(await getEnHpContent()),
   };
 }
--- src/pages/api/hp/it.json.ts
+++ src/pages/api/hp/it.json.ts
@@ -3,31 +3,33 @@ import type { HpContent } from '../../../@types/hp';
 
 const lng = 'it';
 
-export const itHpContent: Readonly<HpContent> = {
-  title: 'La tech community di Javascript su Roma',
-  description: 'Ci vediamo il prossimo terzo mercoledì del mese!',
-  sections: [
-    {
-      heading: l10n('nextTalkTitle', { lng }),
-      body: 'How Open Source changed my life & Video editing con js - RomaJS @ Sourcesense',
-      cta: {
-        href: 'https://www.meetup.com/romajs/events/293424465/',
-        text: l10n('ctaRegister', { lng }),
+export async function getHpItContent(): Promise<Readonly<HpContent>> {
+  return {
+    title: 'La tech community di Javascript su Roma',
+    description: 'Ci vediamo il prossimo terzo mercoledì del mese!',
+    sections: [
+      {
+        heading: l10n('nextTalkTitle', { lng }),
+        body: 'How Open Source changed my life & Video editing con js - RomaJS @ Sourcesense',
+        cta: {
+          href: 'https://www.meetup.com/romajs/events/293424465/',
+          text: l10n('ctaRegister', { lng }),
+        },
       },
-    },
-    {
-      heading: l10n('latestTalkTitle', { lng }),
-      body: 'Attack on NPM with Alessandro Miliucci',
-      cta: {
-        href: 'https://youtu.be/BSKXpTFzKf4?t=384',
-        text: l10n('ctaVOD', { lng }),
+      {
+        heading: l10n('latestTalkTitle', { lng }),
+        body: 'Attack on NPM with Alessandro Miliucci',
+        cta: {
+          href: 'https://youtu.be/BSKXpTFzKf4?t=384',
+          text: l10n('ctaVOD', { lng }),
+        },
       },
-    },
-  ],
-};
+    ],
+  };
+}
 
 export async function get() {
   return {
-    body: JSON.stringify(itHpContent),
+    body: JSON.stringify(await getHpItContent()),
   };
 }
