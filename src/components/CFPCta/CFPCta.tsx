b523e35f83b4a152fea564f33e1cb88197f9efb7
FaberVitale <faber.vitale@gmail.com>
2023-08-11 10:17:07 +0200

    feat: implement design of upcoming events page

---
+++ src/components/CFPCta/CFPCta.tsx
@@ -0,0 +1,30 @@
+import { splitProps, type JSX } from 'solid-js';
+import styles from './cfpCta.module.scss';
+
+export function CFPCta(
+  props: Omit<JSX.IntrinsicElements['a'], 'children' | 'href'>
+) {
+  const [local, otherProps] = splitProps(props, ['class', 'classList']);
+
+  const classList = () => {
+    const output = { ...local.classList };
+
+    if (local.class) {
+      output[local.class] = true;
+    }
+
+    return output;
+  };
+
+  return (
+    <a
+      class={styles.cta}
+      classList={classList()}
+      rel="noopener noreferrer"
+      target="_blank"
+      href={import.meta.env.PUBLIC_CFP_FORM_HREF}
+    >
+      CFP Open{' '}
+    </a>
+  );
+}
