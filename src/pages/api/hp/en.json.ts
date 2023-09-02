8f1fcc4de047da08ce7757841cdb5c228c3bc268
FaberVitale <faber.vitale@gmail.com>
2023-09-02 11:13:00 +0200

    feat: 2023-09 content update HP and upcoming-events page

--- src/pages/api/hp/en.json.ts
+++ src/pages/api/hp/en.json.ts
@@ -10,18 +10,10 @@ export async function getEnHpContent(): Promise<Readonly<HpContent>> {
     sections: [
       {
         heading: l10n('nextTalkTitle', { lng }),
-        body: 'How Open Source changed my life & Video editing con js - RomaJS @ Sourcesense',
+        body: 'RomaJS - Open Source night hack @ SourceSense',
         cta: {
-          href: 'https://www.meetup.com/romajs/events/293424465/',
+          href: 'https://www.meetup.com/romajs/events/295438372/',
           text: l10n('ctaRegister', { lng }),
-        },
-      },
-      {
-        heading: l10n('latestTalkTitle', { lng }),
-        body: 'Attack on NPM with Alessandro Miliucci',
-        cta: {
-          href: 'https://youtu.be/BSKXpTFzKf4?t=384',
-          text: l10n('ctaVOD', { lng }),
         },
       },
     ],
