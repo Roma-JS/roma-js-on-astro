8b81688f90a68f7a10ea3c0ea399a1eef28493bd
FaberVitale <faber.vitale@gmail.com>
2024-03-09 11:18:08 +0100

    fix(GH-70): placeholder event of meetup of current month not always showing up

--- src/components/EventsList/EventsList.tsx
+++ src/components/EventsList/EventsList.tsx
@@ -1,50 +1,53 @@
 import type { JSX } from 'solid-js/jsx-runtime';
 import styles from './styles.module.scss';
-import type { MeetupEventType } from '@api/meetup/event.graqhql.types';
-import { For } from 'solid-js';
+import { For, Show } from 'solid-js';
 import type { Lang } from '@i18n/types';
 import { CFPCta } from '@components/CFPCta/CFPCta';
 import { formatDate } from '@i18n/date-time';
+import type { UpcomingEvent } from 'utils/meetup-events';
 
 export interface EventsListProps {
-  data?: MeetupEventType[] | null;
-  upcoming?: Date[];
+  events?: UpcomingEvent[];
   lang: Lang;
 }
 
-export function EventsList({
-  data,
-  upcoming,
-  lang,
-}: EventsListProps): JSX.Element {
+export function EventsList({ events, lang }: EventsListProps): JSX.Element {
   return (
     <ul class={styles.eventsList}>
-      <For each={data}>
+      <For each={events}>
         {(event) => (
-          <li>
-            <section class={styles.event}>
-              <time datetime={new Date(event.dateTime).toISOString()}>
-                {formatDate(lang, new Date(event.dateTime))}
-              </time>
-              <h3 class={styles.eventHeading}>
-                <a href={event.eventUrl}>{event.title}</a>
-              </h3>
-            </section>
-          </li>
-        )}
-      </For>
-      <For each={upcoming}>
-        {(event) => (
-          <li>
-            <section class={styles.event}>
-              <time datetime={new Date(event).toISOString()}>
-                {formatDate(lang, new Date(event))}
-              </time>
-              <h3 class={styles.eventHeading}>
-                TBD <CFPCta />
-              </h3>
-            </section>
-          </li>
+          <>
+            <Show when={event.type === 'placeholder' && event.date}>
+              {(placeholderDate) => (
+                <li>
+                  <section class={styles.event}>
+                    <time datetime={placeholderDate().toISOString()}>
+                      {formatDate(lang, placeholderDate())}
+                    </time>
+                    <h3 class={styles.eventHeading}>
+                      TBD <CFPCta />
+                    </h3>
+                  </section>
+                </li>
+              )}
+            </Show>
+            <Show when={event.type === 'scheduled' && event.data}>
+              {(scheduled) => (
+                <li>
+                  <section class={styles.event}>
+                    <time
+                      datetime={new Date(scheduled().dateTime).toISOString()}
+                    >
+                      {formatDate(lang, new Date(scheduled().dateTime))}
+                    </time>
+                    <h3 class={styles.eventHeading}>
+                      <a href={scheduled().eventUrl}>{scheduled().title}</a>
+                    </h3>
+                  </section>
+                </li>
+              )}
+            </Show>
+          </>
         )}
       </For>
     </ul>
--- src/utils/meetup-events.ts
+++ src/utils/meetup-events.ts
@@ -1,4 +1,12 @@
 import type { MeetupEventType } from '@api/meetup/event.graqhql.types';
+import {
+  startOfMonth,
+  nextWednesday,
+  isWednesday,
+  isSameDay,
+  isBefore,
+  addMonths,
+} from 'date-fns';
 
 export interface MeetupEventsApiResponse {
   events: Pick<
@@ -6,6 +14,19 @@ export interface MeetupEventsApiResponse {
     'id' | 'title' | 'dateTime' | 'eventUrl' | 'group' | 'duration' | 'venue'
   >[];
 }
+
+type ScheduledEvent = {
+  type: 'scheduled';
+  data: MeetupEventType;
+  epochTimeMs: number;
+};
+type PlaceholderEvent = {
+  type: 'placeholder';
+  date: Date;
+  epochTimeMs: number;
+};
+
+export type UpcomingEvent = ScheduledEvent | PlaceholderEvent;
 
 export function computeMeetupEventsApiResponse(
   meetupEvents: MeetupEventType[] | null | undefined
@@ -27,3 +48,70 @@ export function computeMeetupEventsApiResponse(
 
 export const pastEventsPageSise =
   parseInt(import.meta.env.PUBLIC_MEETUP_PAGE_SIZE, 10) || 10;
+
+/**
+ * Computes the 3rd Wednesday of the month of the input date
+ * and returns it as a Date.
+ * @param date input date.
+ */
+export function computeThirdWednesdayOfMonth(date: Date | number): Date {
+  const firstDayOfNextMonth = startOfMonth(date);
+
+  if (isWednesday(firstDayOfNextMonth)) {
+    return nextWednesday(nextWednesday(firstDayOfNextMonth));
+  }
+
+  return nextWednesday(nextWednesday(nextWednesday(firstDayOfNextMonth)));
+}
+
+/**
+ * Given a list of scheduled events, returns a list of scheduled and placeholder
+ * events sorted by date in ascending order.
+ * @param scheduledUpcomingEvents Scheduled events
+ * @param curentEpochTimeMs built time epoch time i.e. Date.now().
+ * @see {@link UpcomingEvent}
+ */
+export function computeUpcomingEvents(
+  scheduledUpcomingEvents: MeetupEventType[] | undefined | null,
+  curentEpochTimeMs: number
+): UpcomingEvent[] {
+  const scheduledEvents =
+    scheduledUpcomingEvents?.map(
+      (data): ScheduledEvent => ({
+        type: 'scheduled',
+        data,
+        epochTimeMs: new Date(data.dateTime).getTime(),
+      })
+    ) || [];
+
+  const meetupOfThisMonth = computeThirdWednesdayOfMonth(curentEpochTimeMs);
+  const isNowBeforeMeetup =
+    !isSameDay(curentEpochTimeMs, meetupOfThisMonth) &&
+    isBefore(curentEpochTimeMs, meetupOfThisMonth);
+
+  const placeholderMonths = isNowBeforeMeetup ? [0, 1, 2, 3] : [1, 2, 3];
+
+  const placeholderEvents = placeholderMonths
+    .map((month): PlaceholderEvent => {
+      const date = computeThirdWednesdayOfMonth(
+        addMonths(new Date(curentEpochTimeMs), month)
+      );
+
+      return {
+        type: 'placeholder',
+        date,
+        epochTimeMs: date.getTime(),
+      };
+    })
+    .filter(
+      (placeholder) =>
+        !scheduledEvents.some(
+          (scheduled) =>
+            isSameDay(scheduled.epochTimeMs, placeholder.epochTimeMs) // filter out placeholder if overlaps with a scheduled event
+        )
+    );
+
+  return (scheduledEvents as UpcomingEvent[])
+    .concat(placeholderEvents)
+    .sort((a, b) => a.epochTimeMs - b.epochTimeMs);
+}
