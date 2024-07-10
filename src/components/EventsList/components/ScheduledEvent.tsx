e124eb83a3e7ad8370ae82f5f2a566db54fe9ccd
FaberVitale <faber.vitale@gmail.com>
2024-07-10 15:38:39 +0200

    fix(a11y): skipped heading level in upcoming and scheduled pages

--- src/components/EventsList/components/ScheduledEvent.tsx
+++ src/components/EventsList/components/ScheduledEvent.tsx
@@ -3,11 +3,15 @@ import type { JSX } from 'solid-js/jsx-runtime';
 import styles from '../styles.module.scss';
 import { formatDate } from '@i18n/date-time';
 import type { MeetupEventType } from '@api/meetup/event.graqhql.types';
+import { Dynamic } from 'solid-js/web';
 
 export interface ScheduleEventProps {
   event: MeetupEventType;
   lang: Lang;
+  heading?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 }
+
+const defaultHeading: NonNullable<ScheduleEventProps['heading']> = 'h2';
 
 export function ScheduledEvent(props: ScheduleEventProps): JSX.Element {
   return (
@@ -15,9 +19,12 @@ export function ScheduledEvent(props: ScheduleEventProps): JSX.Element {
       <time datetime={new Date(props.event.dateTime).toISOString()}>
         {formatDate(props.lang, new Date(props.event.dateTime))}
       </time>
-      <h3 class={styles.eventHeading}>
+      <Dynamic
+        component={props.heading ?? defaultHeading}
+        class={styles.eventHeading}
+      >
         <a href={props.event.eventUrl}>{props.event.title}</a>
-      </h3>
+      </Dynamic>
     </section>
   );
 }
