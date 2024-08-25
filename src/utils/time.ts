b1a27e814a3d678972c3fcfce84026a9d241eeb1
FaberVitale <faber.vitale@gmail.com>
2024-08-25 14:32:05 +0200

    chore: add parseMonthsList

---
+++ src/utils/time.ts
@@ -0,0 +1,56 @@
+import { parse as parseDate } from 'date-fns';
+
+const monthFormat = 'LL';
+
+export type ParseMonthsListOutput = {
+  /**
+   * Set of **0-based** index months.
+   */
+  readonly parsedMonths: Set<number>;
+  readonly errors: { input: string; error: unknown }[];
+};
+
+/**
+ * Parses a comma separated list of months.
+ *
+ * Returns an output object containing a set of **0-based** index months.
+ *
+ * @param months a comma separated list of months.
+ * @returns {ParseMonthsListOutput} an output object
+ */
+export const parseMonthsList = (
+  months: string | undefined | null
+): ParseMonthsListOutput => {
+  const formattedMonths =
+    months
+      ?.trim()
+      .split(/\s*,\s*/)
+      .filter(Boolean) ?? [];
+  const referenceDate = new Date(new Date().getFullYear(), 0, 4);
+
+  const output: ParseMonthsListOutput = {
+    parsedMonths: new Set<number>(),
+    errors: [],
+  };
+
+  for (const formattedMonth of formattedMonths) {
+    try {
+      const parsedDate = parseDate(formattedMonth, monthFormat, referenceDate);
+
+      if (Number.isFinite(parsedDate.getTime())) {
+        output.parsedMonths.add(parsedDate.getMonth());
+      } else {
+        throw new Error(
+          `cannot parse date using format=${monthFormat}, input=${formattedMonth}`
+        );
+      }
+    } catch (err) {
+      output.errors.push({
+        input: formattedMonth,
+        error: err,
+      });
+    }
+  }
+
+  return output;
+};
